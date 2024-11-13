c37c344ad3b116e8132a6e0d38ed8a6528dbef01
Jillian FitzMaurice <fitzmauricejillian@gmail.com>
2024-11-12 19:12:18 -0700

    basic info added leopard

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ function showPage(pageId) {
     showPage('home');
   });
   
+
+  let currentSlide = 0;
+  const slides = document.querySelectorAll(".carousel img");
+
+  function showSlide(index) {
+    slides[currentSlide].classList.remove("active");
+    currentSlide = (index + slides.length) % slides.length;
+    slides[currentSlide].classList.add("active");
+  }
+
+  function nextSlide() {
+    showSlide(currentSlide + 1);
+  }
+
+  function prevSlide() {
+    showSlide(currentSlide - 1);
+  }
